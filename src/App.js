cfce5121c5eafcb19c85ddfe855b1db1b51317e3
cherrydub <cherrydub@gmail.com>
2023-01-17 20:49:22 +0000

    added searchfield, #234 on Udemy

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import React from "react";
+import React, { Component } from "react";
 import CardList from "./CardList.js";
 import { robots } from "./robots";
 import SearchBox from "./SearchBox";
 
-
-
 const state = {
-    robots: robots,
-    searchfield: ''
-}
-
-
-const App = () => {
-  return (
-    <div className="tc">
-      <h1>RoboFriendsz</h1>
-      <SearchBox />
-      <CardList robots={robots} />
-    </div>
-  );
+  robots: robots,
+  searchfield: ``,
 };
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      robots: robots,
+      searchfield: ``,
+    };
+  }
+
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value });
+  };
+
+  render() {
+    const filteredRobots = this.state.robots.filter((robot) => {
+      return robot.name
+        .toLowerCase()
+        .includes(this.state.searchfield.toLowerCase());
+    });
+    return (
+      <div className="tc">
+        <h1>RoboFriends</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <CardList robots={filteredRobots} />
+      </div>
+    );
+  }
+}
 
 export default App;
