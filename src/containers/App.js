6d6100243c104f933db7e2fd8fbe505fb034d864
cherrydub <cherrydub@gmail.com>
2023-02-01 10:50:50 +0000

    added npm run build

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,6 +4,7 @@ import { robots } from "../robots";
 import SearchBox from "../components/SearchBox";
 import "./App.css";
 import Scroll from "../components/Scroll";
+import ErrorBoundry from "../components/ErrorBoundry.js";
 
 class App extends Component {
   constructor() {
@@ -38,7 +39,9 @@ class App extends Component {
         <h1 className="f1">RoboFriends</h1>
         <SearchBox searchChange={this.onSearchChange} />
         <Scroll>
-          <CardList robots={filteredRobots} />
+          <ErrorBoundry>
+            <CardList robots={filteredRobots} />
+          </ErrorBoundry>
         </Scroll>
       </div>
     );
